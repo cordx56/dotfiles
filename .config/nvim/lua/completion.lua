3ed57efe6b3527bd10cf281a2ccc00fd98fb4b56
CHISEN Kaoru <cordx56@cordx.cx>
2023-07-25 13:28:17 +0900

    use new completion

--- .config/nvim/lua/completion.lua
+++ .config/nvim/lua/completion.lua
@@ -1,9 +1,14 @@
 local cmp = require("cmp")
 
 cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        ["<tab>"] = cmp.mapping.select_next_item(),
+        ["<s-tab>"] = cmp.mapping.select_prev_item(),
+        ["<cr>"] = cmp.mapping.confirm({ select = true }),
+    }),
     sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "buffer" },
         { name = "path" },
-    })
+    }),
 })
--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -13,14 +13,6 @@ nnoremap("<space>e", vim.diagnostic.open_float)
 -- LSP
 on_lsp_attach(function(ev)
     local opts = { buffer = ev.buf }
-    local opts_expr = vim.tbl_extend("force", opts, { expr = true })
-    inoremap("<tab>", function()
-        return vim.fn.pumvisible() == 1 and "<c-n>" or "<tab>"
-    end, opts_expr)
-    inoremap("<s-tab>", function()
-        return vim.fn.pumvisible() == 1 and "<c-p>" or "<s-tab>"
-    end, opts_expr)
-    inoremap("jc", "<c-x><c-o>")
     nnoremap("gd", vim.lsp.buf.definition, opts)
     nnoremap("<space>h", vim.lsp.buf.hover, opts)
     nnoremap("F", function()
