b7bfdd745c570eefbcfe4e39440178f5a6cb42de
CHISEN Kaoru <cordx56@cordx.cx>
2023-09-14 23:11:25 +0900

    implement git repo relative filename statusline

--- .config/nvim/lua/line.lua
+++ .config/nvim/lua/line.lua
@@ -1,28 +1,27 @@
 local set_filepath_from_git_repo = function()
-    vim.system({"git", "rev-parse", "--show-toplevel"}, { text = true }, function(res)
-        local path = vim.api.nvim_buf_get_name(0)
-        if res.code == 0 then
-            local git_dir = vim.trim(res.stdout)
-            vim.b.filepath_from_git_repo = string.sub(path, string.len(git_dir) + 2)
-        else
-            vim.b.filepath_from_git_repo = path
-        end
-    end)
+    local path = vim.api.nvim_buf_get_name(0)
+    local git_dir = vim.trim(vim.fn.system({"git", "rev-parse", "--show-toplevel"}))
+    local index = string.find(path, git_dir, 1, true)
+    if index == 1 then
+        vim.b.filepath_from_git_repo = string.sub(path, string.len(git_dir) + 2)
+    else
+        vim.b.filepath_from_git_repo = path
+    end
 end
-vim.api.nvim_create_autocmd({"BufAdd", "BufFilePost"}, {
+vim.api.nvim_create_autocmd({"BufAdd", "BufEnter", "BufFilePost"}, {
     callback = set_filepath_from_git_repo,
 })
 
 require("lualine").setup {
     options = {
         theme = "horizon",
-        sections = {
-            lualine_a = {"mode"},
-            lualine_b = {"branch", "diff", "diagnostics"},
-            lualine_c = {"b:filepath_from_git_repo"},
-            lualine_x = {"encoding", "fileformat", "filetype"},
-            lualine_y = {},
-            lualine_z = {"progress", "location"},
-        },
+    },
+    sections = {
+        lualine_a = {"mode"},
+        lualine_b = {"branch", "diff", "diagnostics"},
+        lualine_c = {"b:filepath_from_git_repo"},
+        lualine_x = {"encoding", "fileformat", "filetype"},
+        lualine_y = {},
+        lualine_z = {"progress", "location"},
     },
 }
