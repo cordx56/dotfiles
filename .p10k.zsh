ac951a1b8c271e9ebfdf8ffe848ba3143072dc7e
CHISEN Kaoru <cordx56@cordx.cx>
2025-05-21 17:50:37 +0900

    display time on shell right

--- .p10k.zsh
+++ .p10k.zsh
@@ -103,6 +103,7 @@
     # time                  # current time
     # =========================[ Line #2 ]=========================
     newline                 # \n
+    time
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
